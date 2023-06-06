8c0865239089b0e350eb20096d9c4d38f2eb422b
Mehmet Agâh <58531248+Aghefendi@users.noreply.github.com>
2023-06-06 13:20:56 +0300

    Patika

---
+++ Patika/hata_yonetim/hata_yonetim/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hata_yonetim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz.");
+                int sayi = Convert.ToInt32(Console.Read());
+                Console.WriteLine(sayi);
+            }
+
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message.ToString());
+
+            }
+            finally
+            {
+
+                Console.WriteLine("işlem tamamlandı...");
+
+            }
+
+            try
+            {
+
+                int a = int.Parse(null);
+            }
+
+
+            catch (ArgumentNullException ex) //formatexception--overflowException--
+            {
+                Console.WriteLine("Boş deger girdiniz...");
+                Console.WriteLine(ex);
+
+            }
+        }
+    }
+}
---
+++ Patika/hata_yonetim/hata_yonetim/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("hata_yonetim")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("hata_yonetim")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("56ce6ca9-d4a4-4eee-8773-2e0464db0502")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
